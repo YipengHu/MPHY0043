76e2e5d770febcc34a26d2781df87a630b70abd1
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2021-09-20 20:55:03 +0100

    ref #8 utils script added

---
+++ tutorials/pointset/utils.py
@@ -0,0 +1,11 @@
+
+import tensorflow as tf
+
+def normalise_pointset(ps, scale, disp):
+    return (tf.transpose(ps,[2,0,1]) + disp[None,None,]) * scale
+
+
+def random_affine_transform(num=1,scale=0.1):
+    corners = tf.reshape(tf.stack(tf.meshgrid([0.,1.],[0.,1.],[0.,1.]),axis=3),[-1,3])[None,...]
+    corners_new = corners + tf.random.uniform((num,8,3),-1,1)*scale
+    return tf.linalg.lstsq(tf.repeat(corners,num,0),corners_new)
