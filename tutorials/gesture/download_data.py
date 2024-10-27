93d461776e331046660e92863183c957900f6414
Yipeng Hu <yipeng.hu@ucl.ac.uk>
2024-10-27 10:47:32 +0000

    ref #15 link to github datasets updated

--- tutorials/gesture/download_data.py
+++ tutorials/gesture/download_data.py
@@ -12,16 +12,20 @@ if os.path.exists(DATA_PATH):
 os.mkdir(DATA_PATH)
 
 
-print('Downloading...')
-url = 'https://weisslab.cs.ucl.ac.uk/WEISSTeaching/datasets/-/raw/jigsaws/video-meta.zip' 
+print('Downloading and extracting data...')
+url = 'https://github.com/yipenghu/datasets/archive/refs/heads/jigsaw.zip' 
 r = requests.get(url,allow_redirects=True)
-zip_file = os.path.join(DATA_PATH,'video-meta.zip')
-_ = open(zip_file,'wb').write(r.content)
+temp_file = 'temp.zip'
+_ = open(temp_file,'wb').write(r.content)
 
-
-print('Extracting...')
-with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+with zipfile.ZipFile(temp_file, 'r') as zip_ref:
     zip_ref.extractall(DATA_PATH)
-os.remove(zip_file)
+os.remove(temp_file)
+print('Done.')
 
-print('JIGSAWS video data downloaded and extracted in: %s' % os.path.abspath(DATA_PATH))
+SOURCE_PATH = os.path.abspath(os.path.join(DATA_PATH,'datasets-jigsaw/video-meta'))
+for item in os.listdir(SOURCE_PATH):
+    shutil.move(os.path.join(SOURCE_PATH, item), os.path.join(DATA_PATH, item))
+shutil.rmtree(os.path.abspath(os.path.join(DATA_PATH,'datasets-jigsaw')))
+
+print('JIGSAWS video data downloaded and extracted: %s' % DATA_PATH)
